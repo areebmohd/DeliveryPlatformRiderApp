5cad169945727aed7911f7f363582e1f4c90257d
areebmohd <areebmohd683@gmail.com>
2026-03-22 10:23:25 +0530

    added maps

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -1,4 +1,8 @@
 declare module '@env' {
   export const SUPABASE_URL: string;
   export const SUPABASE_ANON_KEY: string;
+  export const MAP_SDK_KEY: string;
+  export const REST_API_KEY: string;
+  export const ATLAS_CLIENT_ID: string;
+  export const ATLAS_CLIENT_SECRET: string;
 }
